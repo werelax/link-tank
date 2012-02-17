0a55b113b52e264ff6d4c624c98ff8bb17f05d7d
Elías Alonso <eliasagc@gmail.com>
2012-02-17 16:39:29 +0100

    renaming servers (confusing)

--- server.rb
+++ server.rb
@@ -1,23 +1,21 @@
-require 'goliath'
-require 'em-synchrony/em-http'
-require 'digest/sha1'
+require 'haml'
+require 'sinatra'
+require 'sprockets'
+require 'coffee-script'
+require './lib/ehaml_template.rb'
 
-# Config vars
-
-CONSUMER_KEY = '3s01qXLnUxyfDxg0xlpXA'
-CONSUMER_SECRET = '27PzjAK0ClxsavLMghvuMS5ieQsbujGs52EaaA3k0'
-
-# Step1: Super-simple twitter login
-
-class Test < Goliath::API
-  use Goliath::Rack::Params
-  use Goliath::Rack::Validations::RequiredParam, key: 'd'
-
-  def response(env)
-    data = params['d']
-  end
+get '/assets/javascripts/application.js' do
+  env = Sprockets::Environment.new
+  env.append_path 'assets/javascripts'
+  env['application.js'].to_s
 end
 
-class GoliExp < Goliath::API
-  post '/test', Test
+get '/assets/stylesheets/application.css' do
+  env = Sprockets::Environment.new
+  env.append_path 'assets/stylesheets'
+  env['application.css'].to_s
+end
+
+get '/' do
+  haml :index
 end
